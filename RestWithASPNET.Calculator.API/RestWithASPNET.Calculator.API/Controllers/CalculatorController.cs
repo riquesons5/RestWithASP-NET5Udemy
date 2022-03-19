07f10c82a537a86bdc66dce61698dcece3808de4
Henrique Franco <henrique.franco11@yahoo.com.br>
2022-03-18 23:54:40 -0300

    create calculator API

---
+++ RestWithASPNET.Calculator.API/RestWithASPNET.Calculator.API/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace RestWithASPNET.Calculator.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculatorController : ControllerBase
+    {
+        private readonly ILogger<CalculatorController> _logger;
+
+        public CalculatorController(ILogger<CalculatorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Invalid Input");
+        }
+
+        private int ConvertToDecimal(string number)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool IsNumeric(string number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
