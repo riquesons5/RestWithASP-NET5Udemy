297f199f9c595747f82e29695adf01ae9a474a29
Henrique Franco <henrique.franco11@yahoo.com.br>
2022-03-19 13:02:03 -0300

    rafatoring

--- RestWithASPNET.Calculator.API/RestWithASPNET.Calculator.API/Controllers/CalculatorController.cs
+++ RestWithASPNET.Calculator.API/RestWithASPNET.Calculator.API/Controllers/CalculatorController.cs
@@ -9,90 +9,101 @@ namespace RestWithASPNET.Calculator.API.Controllers
     [Route("[controller]")]
     public class CalculatorController : ControllerBase
     {
-        private readonly ILogger<CalculatorController> _logger;
-
-        public CalculatorController(ILogger<CalculatorController> logger)
-        {
-            _logger = logger;
-        }
-
         [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
-            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            try
             {
-                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString());
-            }
+                var result = Operator(EnumOperator.sum, firstNumber, secondNumber);
 
-            return BadRequest("Invalid Input");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
         public IActionResult Subtraction(string firstNumber, string secondNumber)
         {
-            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            try
             {
-                var sum = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString());
-            }
+                var result = Operator(EnumOperator.subtraction, firstNumber, secondNumber);
 
-            return BadRequest("Invalid Input");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
         public IActionResult Multiplication(string firstNumber, string secondNumber)
         {
-            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            try
             {
-                var sum = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString());
-            }
+                var result = Operator(EnumOperator.multiplication, firstNumber, secondNumber);
 
-            return BadRequest("Invalid Input");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("division/{firstNumber}/{secondNumber}")]
         public IActionResult Division(string firstNumber, string secondNumber)
         {
-            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            try
             {
-                var sum = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
-                return Ok(sum.ToString());
-            }
+                var result = Operator(EnumOperator.division, firstNumber, secondNumber);
 
-            return BadRequest("Invalid Input");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("mean/{firstNumber}/{secondNumber}")]
         public IActionResult Mean(string firstNumber, string secondNumber)
         {
-            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            try
             {
-                var sum = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber))/2;
-                return Ok(sum.ToString());
-            }
+                var result = Operator(EnumOperator.mean, firstNumber, secondNumber);
 
-            return BadRequest("Invalid Input");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         [HttpGet("square-root/{firstNumber}")]
         public IActionResult SquareRoot(string firstNumber)
         {
-            if (IsNumeric(firstNumber))
+            try
             {
-                var squareRoot = Math.Sqrt((double)ConvertToDecimal(firstNumber));
-                return Ok(squareRoot.ToString());
-            }
+                var result = Operator(EnumOperator.squareRoot, firstNumber);
 
-            return BadRequest("Invalid Input");
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
-
+        #region rules
         private decimal ConvertToDecimal(string strNumber)
         {
             decimal decimalValue;
-            if(decimal.TryParse(strNumber, out decimalValue))
+            if (decimal.TryParse(strNumber, out decimalValue))
             {
                 return decimalValue;
             }
@@ -105,5 +116,42 @@ namespace RestWithASPNET.Calculator.API.Controllers
             bool isNumber = double.TryParse(strNumber, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out number);
             return isNumber;
         }
+
+        private string Operator(EnumOperator operationType, params string[] numbers)
+        {
+            foreach (string number in numbers)
+            {
+                if (!IsNumeric(number)) throw new Exception("Operador inválido!");
+            }
+
+            switch (operationType)
+            {
+                case EnumOperator.sum:
+                    return (ConvertToDecimal(numbers[0]) + ConvertToDecimal(numbers[1])).ToString();
+                case EnumOperator.subtraction:
+                    return (ConvertToDecimal(numbers[0]) - ConvertToDecimal(numbers[1])).ToString();
+                case EnumOperator.multiplication:
+                    return (ConvertToDecimal(numbers[0]) * ConvertToDecimal(numbers[1])).ToString();
+                case EnumOperator.division:
+                    return (ConvertToDecimal(numbers[0]) / ConvertToDecimal(numbers[1])).ToString();
+                case EnumOperator.mean:
+                    return ((ConvertToDecimal(numbers[0]) + ConvertToDecimal(numbers[1])) / 2).ToString();
+                case EnumOperator.squareRoot:
+                    return (Math.Sqrt((double)ConvertToDecimal(numbers[0]))).ToString();
+                default:
+                    throw new Exception("Operação inválida!");
+            }
+        }
+        #endregion
+    }
+
+    public enum EnumOperator
+    {
+        sum = 1,
+        subtraction = 2,
+        multiplication = 3,
+        division = 4,
+        mean = 6,
+        squareRoot = 7
     }
 }
