837b1a32b725d7bbcb1475f52bd303ead92ac8d3
Henrique Franco <henrique.franco11@yahoo.com.br>
2022-03-20 20:19:05 -0300

    Implementing four verbs

---
+++ RestWithASPNET.Person.API/RestWithASPNET.Person.API/RestWithASPNET.Person.API/Controllers/PersonController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET.Persons.API.Model;
+using RestWithASPNET.Persons.API.Services;
+
+namespace RestWithASPNET.Persons.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.FindAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null) return NotFound();
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] Person person)
+        {
+            if (person == null) return BadRequest();
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            _personService.Delete(id);
+            return NoContent();
+        }
+    }
+}
